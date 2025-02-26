39cbcd5a59ec3183b9952b86d2d96c1123f30f67
irina sementchoukova <ir.sement@gmail.com>
2025-02-26 16:30:30 -0500

    updated poema nd background

---
+++ script.js
@@ -0,0 +1,46 @@
+const poems = [
+  {
+    author: "Cece",
+    poem: "Roses are red \n Violets are blue \n Sunflowers are yellow \n I bet you were expecting something romantic but no -- these are just gardening facts.",
+    image: "sunflower.jpeg",
+  },
+  {
+    author: "Anvit",
+    poem: "Roses are red \n Pizza sauce is too \n I ordered a large \n None of it is for you ",
+    image: "pizza.jpeg",
+  },
+  {
+    author: "Ernie",
+    poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
+    image: "bee.jpeg",
+  },
+  {
+    author: "Irina",
+    poem: "Roses are red \n Violets are blue \n my cat is my favourite \n but you're okay too",
+    image:
+      "https://www.rd.com/wp-content/uploads/2023/04/Roses-Are-Red-Poems-FT-GettyImages-913362778.jpg",
+  },
+];
+
+let displayPoems = (array) => {
+  let html = "";
+  let poemDiv = document.getElementById("poems");
+
+  poemArray = array.forEach((poem) => {
+    html += `
+    <div class="poem">
+        <img class="poem__image" src="./assets/${poem.image}"/>
+        <div class="poem__author">
+            Author: ${poem.author}
+        </div>
+        <div class="poem__content">
+            ${poem.poem}
+        </div>
+    </div>
+    `;
+  });
+
+  poemDiv.innerHTML = html;
+};
+
+displayPoems(poems);
